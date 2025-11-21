1cd3b2a06b9507db268d1bab5fc06471bcfe2a1a
Gustavo Adolfo Lira Salazar <gustavoadolfolira@gmail.com>
2025-11-21 19:23:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3461568476-80e92",
+  "appId": "1:774592495272:web:3d29a1ff07a15fbb054c9d",
+  "apiKey": "AIzaSyCFaF5IGkn32kmN8XZV9Dnh8f58qDkStic",
+  "authDomain": "studio-3461568476-80e92.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "774592495272"
+};
