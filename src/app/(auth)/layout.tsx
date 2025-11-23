d2f63ad10c816ae0b32c3f6c2effdcc5c257e214
Gustavo Adolfo Lira Salazar <gustavoadolfolira@gmail.com>
2025-11-23 16:07:16 +0000

    Corazón en login igual de rojo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout({
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50 p-4">
       <div className="mb-8 flex items-center font-cursive text-4xl text-gray-800">
         <span className="font-bold text-primary">Clar</span>
-        <HeartPulse className="relative -bottom-1 mx-[-2px] h-7 w-7 text-muted-foreground" />
+        <HeartPulse className="relative -bottom-1 mx-[-2px] h-7 w-7 text-primary" />
         <span className="text-foreground">Mi Salud</span>
       </div>
       <main className="w-full max-w-md">{children}</main>
